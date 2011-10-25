75f38eb0d0037eb362120d5e2ddf6a48d11fca4f
Kamil Pluszczewicz <k.pluszczewicz@gmail.com>
2011-10-25 15:07:46 +0200

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -42,4 +42,16 @@ describe PagesController do
     end
   end
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get "help"
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get "help"
+      response.should have_selector("title", :content => " | Help")
+    end
+  end
+
 end
